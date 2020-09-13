6bdfe83fa5c16ca55e69848eed1002921629ed23
jieblue <53696774+jieblue@users.noreply.github.com>
2020-09-13 15:58:19 +0800

    second commit

--- GHAnalysis.py
+++ GHAnalysis.py
@@ -5,13 +5,13 @@ import argparse
 
 
 class Data:
-    def __init__(self,addr:str=None,reload:int=0):
+    def __init__(self,addr:str=None,isfirst:int=0):
         self.uevent = {}
         self.revent = {}
         self.urevent = {}
-        if reload==1:
-            self.totalanalyse(addr)
-            self.savetolocal()
+        if isfirst == 1:
+            self.TotalAnalyse(addr)
+            self.SaveToLocal()
         if addr is None and not os.path.exists('1.json') and not os.path.exists('2.json') and not os.path.exists(
                 '3.json'):
             raise RuntimeError('error: init failed')
@@ -29,17 +29,17 @@ class Data:
         self.localur = json.loads(x)
 
 
-    def totalanalyse(self,addr:str):
+    def TotalAnalyse(self,addr:str):
         for root, dic, files in os.walk("jdata"):
             for f in files:
                 if f[-5:] == ".json":
                     jpath = f
-                    self.analyse(addr,jpath)
+                    self.JsonAnalyse(addr,jpath)
 
 
 
-    def analyse(self,addr:str,jpath:str):
-        f = open(addr+'\\'+jpath,'r', encoding='utf-8')
+    def JsonAnalyse(self,addr:str,jpath:str):
+        f = open(addr + '\\' + jpath,'r', encoding='utf-8')
         #f=open("data.json",'r',encoding='utf-8').read()
         try:
             while True:
@@ -52,11 +52,11 @@ class Data:
                     if not dtmp["type"] in ['PushEvent','IssueCommentEvent','IssuesEvent','PullRequestEvent']:
                         continue
                     if not dtmp["actor"]["login"] in self.uevent.keys():
-                        event={'PushEvent':0,'IssueCommentEvent':0,'IssuesEvent':0,'PullRequestEvent':0}
+                        event = {'PushEvent':0,'IssueCommentEvent':0,'IssuesEvent':0,'PullRequestEvent':0}
                         self.uevent[dtmp["actor"]["login"]] = event
                         #print(uevent.items())
                     if not dtmp["repo"]["name"] in self.revent.keys():
-                        event={'PushEvent':0,'IssueCommentEvent':0,'IssuesEvent':0,'PullRequestEvent':0}
+                        event = {'PushEvent':0,'IssueCommentEvent':0,'IssuesEvent':0,'PullRequestEvent':0}
                         self.revent[dtmp["repo"]["name"]] = event
                         #print(revent.items())
                     if not dtmp["actor"]["login"]+dtmp["repo"]["name"] in self.urevent.keys():
@@ -73,7 +73,9 @@ class Data:
             f.close()
         #print(self.uevent.items())
        # print(revent.items())
-    def savetolocal(self):
+
+
+    def SaveToLocal(self):
 
         with open('1.json', 'w', encoding = 'utf-8') as f:
             json.dump(self.uevent,f)
@@ -81,19 +83,26 @@ class Data:
             json.dump(self.revent,f)
         with open('3.json', 'w', encoding = 'utf-8') as f:
             json.dump(self.urevent,f)
-    def querybyu(self, user:str, event: str):
+
+            
+    def QueryByUser(self, user:str, event: str):
         if not user in self.localu.keys():
             return 0
         return self.localu[user][event]
 
-    def querybyr(self, repo: str, event: str):
+
+    def QueryByRepo(self, repo: str, event: str):
         if not self.localr.get(repo, 0):
             return 0
         return self.localr[repo][event]
-    def querybyur(self,user:str,repo:str,event: str):
+
+
+    def QueryByUserAndRepo(self, user:str, repo:str, event: str):
         if not self.localur.get(user+repo,0):
             return 0
         return self.localur[user+repo][event]
+
+
 class Run:
     def __init__(self):
         self.parser = argparse.ArgumentParser()
@@ -117,13 +126,13 @@ class Run:
             if self.parser.parse_args().event:
                 if self.parser.parse_args().user:
                     if self.parser.parse_args().repo:
-                        res = self.data.querybyur(
+                        res = self.data.QueryByUserAndRepo(
                             self.parser.parse_args().user, self.parser.parse_args().repo, self.parser.parse_args().event)
                     else:
-                        res = self.data.querybyu(
+                        res = self.data.QueryByUser(
                             self.parser.parse_args().user, self.parser.parse_args().event)
                 elif self.parser.parse_args().repo:
-                    res = self.data.querybyr(
+                    res = self.data.QueryByRepo(
                         self.parser.parse_args().repo, self.parser.parse_args().event)
                 else:
                     raise RuntimeError('error: argument -l or -c are required')
@@ -133,8 +142,5 @@ class Run:
 
 
 if __name__ == '__main__':
-    # data=Data("0",1)
-    # print(data.querybyu("jieblue","PushEvent"))
-    # print(data.querybyr("zzz","PushEvent"))
-    #print(data.querybyur("izuzero","izuzero/xe-module-ajaxboard","PushEvent"))
-    run=Run()
+
+    run = Run()
